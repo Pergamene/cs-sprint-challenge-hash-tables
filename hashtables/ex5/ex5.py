f0ce285e28fb5b16f3e872b0179fdd100728a8fb
Pergamene <smartz373@gmail.com>
2020-08-07 10:28:20 -0600

    ex5

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,25 +1,12 @@
-# Your code here
-
-
-
 def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-
-    return result
-
+  queries_dict = dict.fromkeys(queries, 1)
+  results = []
+  for file in files:
+    if file.split('/')[-1] in queries_dict:
+      results.append(file)
+  return results
 
 if __name__ == "__main__":
-    files = [
-        '/bin/foo',
-        '/bin/bar',
-        '/usr/bin/baz'
-    ]
-    queries = [
-        "foo",
-        "qux",
-        "baz"
-    ]
-    print(finder(files, queries))
+  files = ['/bin/foo', '/bin/bar', '/usr/bin/baz']
+  queries = ["foo", "qux", "baz"]
+  print(finder(files, queries))

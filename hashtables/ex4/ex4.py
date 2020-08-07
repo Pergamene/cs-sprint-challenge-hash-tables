61dafa24416a7e49c0f1ce362dcfb01536baa038
Pergamene <smartz373@gmail.com>
2020-08-07 10:03:29 -0600

    ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,12 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-
-    return result
-
+  nums = {}
+  matches = []
+  for num in a:
+    if abs(num) in nums:
+      matches.append(abs(num))
+    else:
+      nums[abs(num)] = 1
+  return matches
 
 if __name__ == "__main__":
-    print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+  print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
